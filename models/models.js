0f16bcf10f97990708fe8bbd877b1328346118e4
Prince Maphupha <princemaphupha@Princes-MacBook-Air.local>
2024-06-19 08:22:09 +0200

    model creation

--- models/models.js
+++ models/models.js
@@ -1,3 +1,12 @@
 import db from "../connection";
 const mongoose = require('mongoose')
 const { Schema } = mongoose
+
+const StockSchema = new Schema({
+    stock: { type: String, required: true },
+    likes: { type: int, required: true }
+})
+
+const Stock = mongoose.model("Stock", StockSchema)
+
+exports.Stock = Stock
