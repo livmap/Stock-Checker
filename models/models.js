4d6551839076c010a35a819d55f772222e764dbf
Prince Maphupha <princemaphupha@Princes-MacBook-Air.local>
2024-06-19 08:19:12 +0200

    connection complete

---
+++ models/models.js
@@ -0,0 +1,3 @@
+import db from "../connection";
+const mongoose = require('mongoose')
+const { Schema } = mongoose
--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,8 @@ module.exports = function (app) {
   app.route('/api/stock-prices')
     .get(function (req, res){
       let { stock, like } = req.query
+
+      
     });
     
 };
